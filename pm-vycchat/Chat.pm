d5a446c0ab67e1f23e10063b7875f99c19f6dc9a
arturaz <arturaz>
2004-06-17 20:35:58 +0000

    Chat.pm
    * Changed encoding to UTF-8.

--- pm-vycchat/Chat.pm
+++ pm-vycchat/Chat.pm
@@ -1432,11 +1432,11 @@ Official web page of Vypress Chat: http://vypress.com/products/chat/
 
 =head1 AUTHOR
 
-Artûras Ðlajus, E<lt>x11@h2o.pieva.netE<gt>
+ArtÅ«ras Å lajus, E<lt>x11@h2o.pieva.netE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2003 by Artûras Ðlajus
+Copyright 2003 by ArtÅ«ras Å lajus
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
